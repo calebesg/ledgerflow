8f4a388a39c0d1992a01b97876a6ac9a8399bf27
calebesg <calebeguimaraes10@gmail.com>
2026-03-04 14:25:55 -0300

    feat: create security filter and token service

--- backend/src/main/java/calebesg/com/github/backend/infrastructure/security/TokenService.java
+++ backend/src/main/java/calebesg/com/github/backend/infrastructure/security/TokenService.java
@@ -1,4 +1,55 @@
 package calebesg.com.github.backend.infrastructure.security;
 
+import calebesg.com.github.backend.domain.user.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
 public class TokenService {
+    private static final String ISSUER = "login-auth-api";
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String createToken(User user) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withIssuer(ISSUER)
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+
+        } catch (Exception error) {
+            throw new RuntimeException("Failed to create token", error);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer(ISSUER)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-3"));
+    }
 }
