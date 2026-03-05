77a3f2f2f80ac70af4b0b6817de595ec3bdb14c8
calebesg <calebeguimaraes10@gmail.com>
2026-03-05 15:40:38 -0300

    refactor: extract create user to UserService

--- backend/src/main/java/calebesg/com/github/backend/services/AuthService.java
+++ backend/src/main/java/calebesg/com/github/backend/services/AuthService.java
@@ -8,13 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final UserService userService;
     private final PasswordEncoder passwordEncoder;
     private final TokenService tokenService;
 
@@ -30,24 +29,8 @@ public class AuthService {
     }
 
     public AuthResponseDTO register(String name, String email, String password) {
-        Optional<User> user = userRepository.findByEmail(email);
-
-        if (user.isPresent()) {
-            throw new RuntimeException("User with email already exists!");
-        }
-
-        if (password.length() < 6) {
-            throw new RuntimeException("Password too short!");
-        }
-
-        var newUser = new User();
-        newUser.setName(name);
-        newUser.setEmail(email);
-        newUser.setPassword(passwordEncoder.encode(password));
-
-        userRepository.save(newUser);
-        String token = tokenService.createToken(newUser);
-
-        return new AuthResponseDTO(token, newUser.getName());
+        User user = userService.createUser(name, email, password);
+        String token = tokenService.createToken(user);
+        return new AuthResponseDTO(token, user.getName());
     }
 }
---
+++ backend/src/main/java/calebesg/com/github/backend/services/UserService.java
@@ -0,0 +1,36 @@
+package calebesg.com.github.backend.services;
+
+import calebesg.com.github.backend.domain.user.User;
+import calebesg.com.github.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User createUser(String name, String email, String password) {
+        Optional<User> user = userRepository.findByEmail(email);
+
+        if (user.isPresent()) {
+            throw new RuntimeException("User with email already exists!");
+        }
+
+        if (password.length() < 6) {
+            throw new RuntimeException("Password too short!");
+        }
+
+        var newUser = new User();
+        newUser.setName(name);
+        newUser.setEmail(email);
+        newUser.setPassword(passwordEncoder.encode(password));
+
+        return userRepository.save(newUser);
+    }
+}
